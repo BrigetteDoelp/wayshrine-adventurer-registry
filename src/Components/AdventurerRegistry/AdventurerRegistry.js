7398fd9d3695c5a2c28565ecf1d13d15631edbeb
Brigette Doelp <brigette.doelp@gmail.com>
2020-11-07 21:46:11 -0700

    Connect api to drop down selections"

--- src/Components/AdventurerRegistry/AdventurerRegistry.js
+++ src/Components/AdventurerRegistry/AdventurerRegistry.js
@@ -6,7 +6,9 @@ class AdventurerRegistry extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      heritages: ''
+      heritages: '',
+      classes: '',
+      subclasses: '',
     }
   }
 
@@ -15,11 +17,37 @@ class AdventurerRegistry extends Component {
     let classList = await getClasses()
     let subClassList = await getSubClasses()
 
-    this.setState({heritages: heritageList})
-    console.log(heritageList, classList, subClassList)
+    this.setState({heritages: heritageList.results})
+    this.setState({classes: classList.results})
+    this.setState({subclasses: subClassList.results})
+  }
+
+  getHeritageNames() {
+    let names = this.state.heritages.map(heritage => {
+      return <option value='question'>{heritage.name}</option>
+    })
+    return names
+  }
+
+  getClassNames() {
+    let names = this.state.classes.map(singleClass => {
+      return <option value='question'>{singleClass.name}</option>
+    })
+    return names
+  }
+
+  getSubClassNames() {
+    let names = this.state.subclasses.map(subclass => {
+      return <option value='question'>{subclass.name}</option>
+    })
+    return names
   }
 
   render() {
+    if (!this.state.heritages || !this.state.classes || !this.state.subclasses) {
+      return <h1>Gathering Registry Information</h1>
+    }
+    console.log(this.state.heritages[0].name)
     return (
       <section>
          <form>
@@ -28,14 +56,17 @@ class AdventurerRegistry extends Component {
           <label>Heritage :</label>
           <select id='heritage' className='dropdown' name='heritage-select'>
             <option value='question'>Choose your heritage</option>
+            {this.getHeritageNames()}
           </select>
           <label>Class :</label>
           <select id='class' className='dropdown' name='class-select'>
             <option value='question'>Choose your class</option>
+            {this.getClassNames()}
           </select>
           <label>SubClass :</label>
           <select id='subclass' className='dropdown' name='destisubclassnation-select'>
             <option value='question'>Choose your subclass</option>
+            {this.getSubClassNames()}
           </select>
           <label>Personal Info :</label>
           <textarea></textarea>
