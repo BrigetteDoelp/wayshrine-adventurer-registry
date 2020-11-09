12029b5ccee435b2e560ba32424f7eafc4f3ca81
Brigette Doelp <brigette.doelp@gmail.com>
2020-11-09 13:50:04 -0700

    Break html elements into smaller pieces for styling

--- src/Components/AdventurerProfile/AdventurerProfile.js
+++ src/Components/AdventurerProfile/AdventurerProfile.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import AdventurerRegistry from '../AdventurerRegistry/AdventurerRegistry.js';
+import './AdventurerProfile.css';
+import skull from '../../Assets/danger.png';
+import heart from '../../Assets/love.png';
 
 const AdventurerProfile = (props) => {
 
   return (
-    <article>
-      <img alt='heart'/>
-      <h3>{props.name}</h3>
-      <img alt='skull'/>
-      <h5>{props.chosenHeritage}</h5>
-      <h5>{props.chosenClass}</h5>
-      <h5>{props.chosenSubClass}</h5>
-      <p>{props.personalInfo}</p>
+    <article className='profileCard'>
+      <section className='top'>
+        <img className='heart' alt='heart' src={heart}/>
+        <h3 className='name'>{props.name}</h3>
+        <img className='skull' alt='skull' src={skull}/>
+      </section>
+      <section className='heritage'>
+        <h5 className='heritage'>{props.chosenHeritage}</h5>
+      </section>
+      <section className='classes'>
+        <h5 className='class'>{props.chosenClass}</h5>
+        <h5 className='subclass'>{props.chosenSubClass}</h5>
+      </section>
+      <p className='personalInfo'>{props.personalInfo}</p>
     </article>
   )
 }
