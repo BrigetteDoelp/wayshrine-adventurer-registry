3873ea5d408f8dc8f86552107e049ac138a556d7
Brigette Doelp <brigette.doelp@gmail.com>
2020-11-09 11:44:42 -0700

    Base style welcome page:

--- src/Components/WelcomePage/WelcomePage.js
+++ src/Components/WelcomePage/WelcomePage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './WelcomePage.css'
 import gsap from 'gsap';
 
 const WelcomePage = () => {
 
   return (
     <section className='welcomePage'>
-      <h1>Adventurer Registry</h1>
+      <h1 className='pageName'>Adventurer Registry</h1>
       <Link to='/registry'>
         <button>Move to Registry</button>
       </Link>
