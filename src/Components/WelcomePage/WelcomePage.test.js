a77bd4b31ab421293e23a4473946ac1ea2fdafec
Brigette Doelp <brigette.doelp@gmail.com>
2020-11-09 17:21:02 -0700

    Add data id for testing

--- src/Components/WelcomePage/WelcomePage.test.js
+++ src/Components/WelcomePage/WelcomePage.test.js
@@ -2,7 +2,6 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import WelcomePage from './WelcomePage';
-import AdventurerRegistry from '../AdventurerRegistry/AdventurerRegistry.js';
 
 describe('WelcomePage', () => {
 
